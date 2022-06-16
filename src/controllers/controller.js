202400834d1ff3556290dc27990109736c0a05c6
Ariel Aguirre <arielferaguirre.2001@gmail.com>
2022-06-16 14:41:59 -0500

    New changes

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -3,4 +3,7 @@ const controller = {}
 controller.index = (req,res) => {
     res.render('index')
 }
+controller.formulario = (req,res) => {
+    res.render('formularioInscripcion')
+}
 module.exports = controller
--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,5 @@ const express = require('express')
 const router = express.Router()
 const controller = require("./controllers/controller")
 router.get('/', controller.index)
+router.get('/formulario',controller.formulario)
 module.exports = router
